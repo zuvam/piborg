5fe9cb9d89408ff9c86bd4382167bc07ee24525b
Zuva <zuva.munshi@gmail.com>
2018-10-12 00:59:39 +0200

    bugfixes

--- wiimote.py
+++ wiimote.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 # coding: Latin-1
-import os
-import sys
-from signal import signal, SIGTERM
 from time import sleep
-
+from signal import signal, SIGTERM
+from motion import MotionController
+import os, sys
+sys.stderr = os.fdopen(os.dup(2), "w")
+os.dup2(os.open(os.devnull, os.O_WRONLY), 2)
 import cwiid
 
-from motion import MotionController
-
-WII_LED = 9  # LEDs to light up on connected wii remote
+WII_LED = 9 # LEDs to light up on connected wii remote
 BTNS_DISCONNECT = cwiid.BTN_PLUS + cwiid.BTN_MINUS
 BTNS_FWD = cwiid.BTN_UP | cwiid.BTN_1
 BTNS_REV = cwiid.BTN_DOWN | cwiid.BTN_2
@@ -20,36 +19,23 @@ BTNS_STOP = cwiid.BTN_B
 BTNS_NO_TURN = cwiid.BTN_HOME
 BTNS_SHUTDOWN = cwiid.BTN_1 + cwiid.BTN_2 + cwiid.BTN_A + cwiid.BTN_B
 
-
 def rumble(wii, n):
     wii.rumble = 1
     sleep(n)
     wii.rumble = 0
 
-
-if len(sys.argv) == 1 or sys.argv[1] != 'start-wii-controller':
-    sys.exit()
-
+if len(sys.argv) == 1 or sys.argv[1] != 'start-wii-controller': sys.exit()
 restart = running = True
-
-
-def sigterm(self, signum, frame):
-    running = restart = False
-
-
+def sigterm(self, signum, frame): running = restart = False
 signal(SIGTERM, sigterm)
-
-
 try:
     wii = None
     linear = angular = 0
     while restart:
         running = True
         while wii is None and running:
-            try:
-                wii = cwiid.Wiimote()
-            except RuntimeError:
-                sleep(1)
+            try: wii = cwiid.Wiimote()
+            except RuntimeError: sleep(1)
         if wii is not None:
             wii.rpt_mode = cwiid.RPT_BTN
             wii.led = WII_LED
@@ -65,23 +51,19 @@ try:
                             if buttons - BTNS_DISCONNECT == 0: running = False
                             if (buttons & BTNS_STOP): linear = 0
                             if (buttons & BTNS_NO_TURN): angular = 0
-                            if (buttons & BTNS_FWD): linear += 1 if linear < 100 else 0
-                            if (buttons & BTNS_REV): linear -= 1 if linear > -100 else 0
-                            if (buttons & BTNS_RIGHT): angular += 1 if angular < 100 else 0
-                            if (buttons & BTNS_LEFT): angular -= 1 if angular > -100 else 0
+                            if (buttons & BTNS_FWD): linear += 1 if linear <100 else 0
+                            if (buttons & BTNS_REV): linear -= 1 if linear >-100 else 0
+                            if (buttons & BTNS_RIGHT): angular += 1 if angular <100 else 0
+                            if (buttons & BTNS_LEFT): angular -= 1 if angular >-100 else 0
                             if angular != 0 and not (buttons & BTNS_TURN):
-                                angular -= abs(angular) / angular
+                                angular -= abs(angular)/angular
                                 mc.set_velocity(linear / 100.0, angular / 100.0)
                             if buttons != 0: mc.set_velocity(linear / 100.0, angular / 100.0)
                             sleep(0.01)
-                        except KeyboardInterrupt:
-                            running = restart = False
-                    mc.set_velocity(0.0, 0.0)
+                        except KeyboardInterrupt: running = restart = False
+                    mc.set_velocity(0.0,0.0)
                     rumble(wii, 0.2)
                     wii = None
-            except IOError:
-                pass
-            except KeyboardInterrupt:
-                running = restart = False
-except KeyboardInterrupt:
-    pass
+            except IOError: pass
+            except KeyboardInterrupt: running = restart = False
+except KeyboardInterrupt: pass
