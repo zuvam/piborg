3f499c25c69a1fb11585551810300f4fcf84aa67
Zuva <zuva.munshi@gmail.com>
2018-10-13 02:01:46 +0200

    changed directory layout and added install/unistall all

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ export DIR=$(dirname $(readlink -f $0))
 sudo ${DIR}/motor_server/install.sh -i
 sudo ${DIR}/wiimotes/install.sh -i
 sudo ${DIR}/camera/install.sh.sh -i
+
--- uninstall.sh
+++ uninstall.sh
@@ -3,3 +3,4 @@ export DIR=$(dirname $(readlink -f $0))
 sudo ${DIR}/camera/install.sh.sh -u
 sudo ${DIR}/wiimotes/install.sh -u
 sudo ${DIR}/motor_server/install.sh -u
+
